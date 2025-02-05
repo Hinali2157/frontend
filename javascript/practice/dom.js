e4ba18164940aa56c3814b3193ab58877b4af836
Hinali2157 <sq.hinali2157@gmail.com>
2025-02-05 17:38:23 +0530

    5-2

--- javascript/practice/dom.js
+++ javascript/practice/dom.js
@@ -47,105 +47,107 @@
 //     let promise = new Promise(function (resolve, reject) {})  
 // }
 
-{
-    let clean = false
-    let krisha = new Promise(function (resolve, reject) {
-        if (clean === true) {
-            resolve('krisha clean the room !!!!')
-        } else {
-            reject('krisha dont clean the room !!!!')
-        }
-    })
-    krisha.then((result) => {
-        console.log(result);
-    })
-        .then(() => {
-            console.log("got a chocolate...!! ");
-        })
-        .then(() => {
-            console.log("krisha is so happy !!!");
-        })
-        .catch((error) => {
-            console.log(error);
-            console.log("krisha go for laundry..!!"); 
-        })
-        .finally(() => {
-            console.log("mamma is happy !!");
-        })
-}
+// {
+//     let clean = false
+//     let krisha = new Promise(function (resolve, reject) {
+//         if (clean === true) {
+//             resolve('krisha clean the room !!!!')
+//         } else {
+//             reject('krisha dont clean the room !!!!')
+//         }
+//     })
+//     krisha.then((result) => {
+//         console.log(result);
+//     })
+//         .then(() => {
+//             console.log("got a chocolate...!! ");
+//         })
+//         .then(() => {
+//             console.log("krisha is so happy !!!");
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//             console.log("krisha go for laundry..!!");
+//         })
+//         .finally(() => {
+//             console.log("mamma is happy !!");
+//         })
+// }
 
 // Javascript Function Closure
 
 
-// function sum(){
-//     return 4 + 4
-// }
-
-// console.log(sum());
-
 // {
-//     function print(name){
-//         function greet(){
-//             console.log('name is' , name);
-//         }
-//         greet();
-//     }
-    
-//     print('skillQode')
-// }
-
-// {
-//     function print(name){
-//         function greet(){
-//             console.log('name is' , name);
-//         }
-//         return greet;
-//     }
-    
-//     let demo = print('skillQode')
-
-//     console.log(demo);
-
-//     console.log(demo());
-// }
-// {
-//     function sum(){
-
-//         let num = 0
-
-//         function multi(){
-//            return num = num + 1
-//         }
-
-//         return multi();
-//     }
-    
-//     console.log(sum()); 
-//     console.log(sum()); 
-// }
-
-// {
-//     function multiply(x){
-//         function add(y){
-//             return x * y
-//         }
-//         return add;
+//     function sum() {
+//         return 4 + 4
 //     }
 
-//     // let demo = multiply(1)
-//     let demo = multiply(2)
-//     let demo2 = multiply(2)
-//     console.log(demo(2));
-//     console.log(demo2(6));
-// }
+//     console.log(sum());
+
+//     {
+//         function print(name) {
+//             function greet() {
+//                 console.log('name is', name);
+//             }
+//             greet();
+//         }
+
+//         print('skillQode')
+//     }
+
+//     {
+//         function print(name) {
+//             function greet() {
+//                 console.log('name is', name);
+//             }
+//             return greet;
+//         }
+
+//         let demo = print('skillQode')
+
+//         console.log(demo);
+
+//         console.log(demo());
+//     }
+//     {
+//         function sum() {
+
+//             let num = 0
+
+//             function multi() {
+//                 return num = num + 1
+//             }
+
+//             return multi();
+//         }
+
+//         console.log(sum());
+//         console.log(sum());
+//     }
+
+//     {
+//         function multiply(x) {
+//             function add(y) {
+//                 return x * y
+//             }
+//             return add;
+//         }
 
 
-// {
-//     if (Math.random() > 0.5) {
-//         var x = 1;
-//       } else {
-//         var x = 2;
-//       }
-//       console.log(x);
-      
+//         let demo = multiply(2)
+//         let demo2 = multiply(2)
+//         console.log(demo(2));
+//         console.log(demo2(6));
+//     }
+
+
+//     {
+//         if (Math.random() > 0.5) {
+//             var x = 1;
+//         } else {
+//             var x = 2;
+//         }
+//         console.log(x);
+
+//     }
 // }
